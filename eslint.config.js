d0b3ef52bb3886105d9bec97b51a55fd27a2979b
rajalishikhe <nahidujjamanraj@gmail.com>
2025-07-20 10:08:20 +0600

    Now I am install  DaisyUi

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,6 +15,7 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
+      node:true,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
