bc51959f00735e73817cce5c5e9291925827a625
rajalishikhe <nahidujjamanraj@gmail.com>
2025-07-22 10:43:56 +0600

    Now my banner section design is ready for large device

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,28 @@
+import Glass from "../Glass/Glass";
 import Navbar from "../Navbar/Navbar";
-
 const Banner = () => {
     return (
-        <div className="min-h-screen bg-[#9538E2] m-8 rounded-xl lg:pr-24 lg:pl-24 md:pr-24 md:pl-24">
-           <Navbar></Navbar>
+        <div> 
+            {/* my banner container  */}
+            <div id="1" className="relative mt-6 lg:pr-6 lg:pl-6 md:pr-6 md:pl-6 mb-96">
+                <div className="min-h-screen bg-[#9538E2]  rounded-xl relative">
+                <Navbar></Navbar>
+                <div className="ml-44 absolute mt-32">
+                     <Glass></Glass>
+               </div>
+               
+                </div>
+                
+               </div>
+               {/* Banner container end  */}
+
+            <div id="2">
+                <h3>1hi</h3>
+            </div>
+
             
         </div>
+        
     );
 };
 
---
+++ src/Components/Glass/Glass.jsx
@@ -0,0 +1,11 @@
+import img from '../../Image/banner.jpg';
+const Glass = () => {
+    return (
+        <div className='border-2 border-white p-4 bg-[#FFFFFF4D] rounded-xl ' >
+            <img className='w-[1110px] h-[511px] ' src={img} alt="" srcset="" />
+        </div>
+    );
+};
+// 
+
+export default Glass;
