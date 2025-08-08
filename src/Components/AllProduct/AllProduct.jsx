94ccd864e8ccba8b17298fb4f0481599752b3150
rajalishikhe <nahidujjamanraj@gmail.com>
2025-08-08 08:50:24 +0600

    I use handleClick for button

--- src/Components/AllProduct/AllProduct.jsx
+++ src/Components/AllProduct/AllProduct.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import ProductContainer from "../ProductContainer/ProductContainer";
 
 const AllProduct = () => {
+    const [buttonName,setButtonName]=useState("")
+    const handleButton=(e)=>{
+        
+        setButtonName(e.target.innerText)
+
+    }
+    console.log("here is my button name ",buttonName)
     return (
         <div  className="flex flex-col  lg:flex-row xl:flex-row 2xl:flex-row gap-6 border sm:justify-center m-2">
             <div className="bg-[#FFFFFF] p-2 rounded-xl flex justify-center h-[400px]">
                 {/* Product Button */}
                <div >
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">All product</button><br />
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Laptop </button><br />
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Phones</button><br />
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Accessories</button><br />
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Smart watches </button><br />
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Mac Book </button><br />
-                 <button className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">I phone </button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">All product</button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Laptop </button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Phones</button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Accessories</button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Smart watches </button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Mac Book </button><br />
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">I phone </button><br />
                  
                  
                </div>
