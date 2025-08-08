22a228fc284daee61c2c4d20b40870504c09e556
rajalishikhe <nahidujjamanraj@gmail.com>
2025-08-08 09:45:05 +0600

    Now I am Show the all product

--- src/Components/AllProduct/AllProduct.jsx
+++ src/Components/AllProduct/AllProduct.jsx
@@ -11,9 +11,10 @@ const AllProduct = () => {
     console.log("here is my button name ",buttonName)
     return (
         <div  className="flex flex-col  lg:flex-row xl:flex-row 2xl:flex-row gap-6 border sm:justify-center m-2">
-            <div className="bg-[#FFFFFF] p-2 rounded-xl flex justify-center h-[400px]">
+            <div className="bg-[#FFFFFF] p-2 rounded-xl flex justify-center h-[470px]">
                 {/* Product Button */}
                <div >
+                 <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Home</button><br />
                  <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">All product</button><br />
                  <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Laptop </button><br />
                  <button onClick={(e)=>handleButton(e)} className="p-3 rounded-xl bg-[#09080F0D] hover:bg-[#9538E2] hover:text-white font-bold mb-2 w-36">Phone</button><br />
--- src/Components/ProductContainer/ProductContainer.jsx
+++ src/Components/ProductContainer/ProductContainer.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
+import toast from 'react-hot-toast';
+import { useLocation } from "react-router-dom";
 import ProductCard from "../ProductCard/ProductCard";
+const notify = () => toast('Here is your toast.');
 
-const ProductContainer = () => {
+const ProductContainer = ({buttonName}) => {
+    const location=useLocation()
+    console.log("My Location",location)
     const [product,setProduct]=useState([])
     const sliceProduct=product.slice(0,9)
     console.log("here is my 9 product",sliceProduct)
@@ -11,10 +16,24 @@ const ProductContainer = () => {
         .then(data=>setProduct(data))
     },[])
     console.log("Here us gadget data ",product)
+
+    // Conditional show the data 
+
+    let renderProducts;
+    if(buttonName === "All product"){
+       
+        renderProducts=product.map(item=><ProductCard item={item}></ProductCard>)
+       
+    }
+    else if(buttonName = "Home"){
+        renderProducts = sliceProduct.map(item=><ProductCard item={item}></ProductCard>)
+    }
+    
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3  gap-4">
+            
            {
-            sliceProduct.map(item=><ProductCard item={item}></ProductCard>)
+             buttonName ?renderProducts: sliceProduct.map(item=><ProductCard item={item}></ProductCard>)
            }
             
         </div>
